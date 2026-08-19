53530da98912b415fdbcde4f56ba34968d235fae
lomadagouthami442-source <lomadagouthami442@gmail.com>
2026-08-19 20:07:53 +0530

    Initialize Express server with registration routes

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = 3001;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.get("/", (req, res) => {
+  res.render("index", { student: null });
+});
+app.post("/register", (req, res) => {
+  const { name, email, course } = req.body;
+  res.render("index", {
+    student: {
+      name,
+      email,
+      course
+    }
+  });
+});
+app.listen(PORT, () => {
+  console.log(Server running at http://localhost:${PORT});
+});
